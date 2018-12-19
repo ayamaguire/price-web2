54efc2f917193ff0a68a683843d4b24472cb9508
Aya Maguire <aya.maguire@gmail.com>
2018-12-19 16:41:18 -0700

    update database to use free mongo lab.

--- src/common/database.py
+++ src/common/database.py
@@ -10,7 +10,7 @@ class Database(object):
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client['fullstack']
+        Database.DATABASE = client['heroku_jns0cx1d']
 
     @staticmethod
     def insert(collection, data):
